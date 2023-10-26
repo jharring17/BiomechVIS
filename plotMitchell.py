1c414d0b00f5e451e72d3720248af4b3d65447c7
gkeating379 <gtkeating@yahoo.com>
2023-10-25 23:12:47 -0400

    Add timeseries function and bad attempt at anatax

--- plotMitchell.py
+++ plotMitchell.py
@@ -11,6 +11,7 @@ import sys
 #TODO the animation to be faster or at least adjustable
 #TODO want a bar for the frame number
 #TODO plot axis
+# note we can use the add trace thing to make it so you can click to show points/groups and lines
 
 def load_from_mat(filename=None, data={}, loaded=None):
     '''Turn .mat file to nested dict of all values
@@ -59,13 +60,13 @@ def read_Mitchell_data():
         points = np.append(points, tag, 1)
         final_points[name] = points
 
-    #make COM dict the same as above
-    #TODO maybe add a distinguishing tag so these can be seen to be COMs and colored accordingly
+    #make COM dict 
+    COMs = {}
     for name in SegCOM:
         points = SegCOM[name]
         tag = np.full((points.shape[0], 1), 0) #0 tag for COMs
         points = np.append(points, tag, 1)
-        final_points[f'{name}'] = points
+        COMs[name] = points
 
     # add TBCM to points
     # df = pd.DataFrame(TBCM)
@@ -82,24 +83,37 @@ def read_Mitchell_data():
     # points['TBCMVeloc'] = df
     #final_points['TBCMVeloc'] = TBCMVeloc
 
-    # add points for AnatAx to points
-    # for ax in AnatAx:
-    #     points = AnatAx[ax]
-    #     df = pd.DataFrame(points)
-    #     df.columns = ['X', 'Y', 'Z']
-    #     points[name] = df
+    # add points for AnatAx to invis points 
+    # structure is key is name points to x,y,z dicts 
+    invis_points = {}
+    for ax in AnatAx:
+        temp = {}
+        temp['X'] = AnatAx[ax][0].T
+        temp['Y'] = AnatAx[ax][1].T
+        temp['Z'] = AnatAx[ax][2].T
+        invis_points[ax] = temp
 
-    return final_points
+    return final_points, COMs, invis_points
 
-def filter_points_to_draw(points, p_filter=[]):
+def filter_points_to_draw(points, COMs, p_filter=[]):
     '''Takes in all points and filters out those in the filter
     Returns one df list.  Each frame is a df at its index
     Returns a list of names for each point in order they are listed in df'''
     frames = []
     labels = []
+    #add mocap points
     for point_name in points:
         if point_name not in p_filter:
             for i, point in enumerate(points[point_name]):
+                if len(frames) <= i:
+                    frames.append([])
+                frames[i].append(point)
+            labels.append(point_name)
+
+    #add COM points
+    for point_name in COMs:
+        if point_name not in p_filter:
+            for i, point in enumerate(COMs[point_name]):
                 if len(frames) <= i:
                     frames.append([])
                 frames[i].append(point)
@@ -113,8 +127,44 @@ def filter_points_to_draw(points, p_filter=[]):
 
     return dfs, labels
 
-def base_plot(dfs, labels):
+def filter_axis_to_draw(axis, a_filer=[]):
+    '''Puts the axis into the dfs setup so they can be plotted invisibily'''
+    frames = []
+    for com_name in axis:
+        if com_name not in a_filer:
+            for dir in axis[com_name]:
+                for i, point in enumerate(axis[com_name][dir]):
+                    if len(frames) <= i:
+                        frames.append([])
+                    frames[i].append(point)
+            
+    frames = np.array(frames)
+    dfs = []
+    for frame in frames:
+        df = pd.DataFrame(frame)
+        df.columns = ['X', 'Y', 'Z']
+        dfs.append(df)
+
+    return dfs
+
+def draw_anat_ax(plot, axes, COMs):
+    '''Draws the lines for each anat ax starting from its corresponding COM'''
+    #TODO not really sure this draws the right lines
+    #TODO this takes forever
+    i = 0
+    for name in COMs:
+        plot = draw_line(plot, COMs[name], axes[name]['X'], 'black')
+        plot = draw_line(plot, COMs[name], axes[name]['Y'], 'red')
+        plot = draw_line(plot, COMs[name], axes[name]['X'], 'yellow')
+        print(i)
+        i += 1
+
+    return plot
+
+
+def base_plot(dfs, labels, invis_dfs):
     '''Takes dfs and labels and returns the plot
+    invis_dfs is the points to plot but not show (used for axis and vectors)
     Each index in dfs is a frame each point in dfs[x] is labeled in order by labels
     returns the plot object'''
     #info for the axis scaling
@@ -136,7 +186,13 @@ def base_plot(dfs, labels):
                             mode='markers', #gets rid of line connecting all points
                             marker={'color':dfs[0]['Segment_ID'], 'size': 5},
                             hovertext= labels
-                            ) #just for frame 1
+                            ),
+            go.Scatter3d(   x=invis_dfs[0]['X'],
+                            y=invis_dfs[0]['Y'], 
+                            z=invis_dfs[0]['Z'],
+                            marker=dict(size=0, opacity=0), #makes them invis
+                            mode='markers', #gets rid of line connecting all points
+                            ), #just for frame 1
         ],
         layout=go.Layout(width=1600, height=800, #TODO dynamically set plot size
                         scene = scene_scaling,
@@ -156,27 +212,36 @@ def base_plot(dfs, labels):
         ),
         frames=[go.Frame(
                 data= [go.Scatter3d(
-                            x=df['X'],
-                            y=df['Y'], 
-                            z=df['Z'], 
+                            x=dfs[i]['X'],
+                            y=dfs[i]['Y'], 
+                            z=dfs[i]['Z'], 
                             mode='markers', #gets rid of line connecting all points
-                            marker={'color':df['Segment_ID'],  'size': 5},
+                            marker={'color':dfs[i]['Segment_ID'],  'size': 5},
                             connectgaps=False, #TODO ask what we should do in this case.  Currently this stops the filling in of blanks/NaNs
                             hovertext = labels
-                            )])
-                for df in dfs] #https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html
+                            ),
+                        go.Scatter3d(
+                            x=invis_dfs[i]['X'],
+                            y=invis_dfs[i]['Y'], 
+                            z=invis_dfs[i]['Z'], 
+                            marker=dict(size=0, opacity=0), #makes them invis
+                            mode='markers', #gets rid of line connecting all points
+                            connectgaps=False, 
+                            ),
+                            ])
+                for i in range(len(dfs))] #https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html
     )
 
     return main_plot
 
 def draw_line(plot, p1, p2, c='red'):
-    '''Add a line in all frames of plot from p1 to p2'''
+    '''Add a line in all frames of plot from p1[x] to p2[x]'''
     #convert point array to df for plotly
-    df = pd.DataFrame(p1)
-    df.columns = ['X', 'Y', 'Z', 'Segment_ID']
+    df = pd.DataFrame(p1[:,:3])
+    df.columns = ['X', 'Y', 'Z']
     p1 = df
-    df = pd.DataFrame(p2)
-    df.columns = ['X', 'Y', 'Z', 'Segment_ID']
+    df = pd.DataFrame(p2[:,:3])
+    df.columns = ['X', 'Y', 'Z']
     p2 = df
 
     plot.add_trace(go.Scatter3d(
@@ -193,10 +258,37 @@ def draw_line(plot, p1, p2, c='red'):
 
     return plot
 
-points = read_Mitchell_data()
-dfs, labels = filter_points_to_draw(points)
-main_plot = base_plot(dfs, labels)
-main_plot = draw_line(main_plot, points['PELVIS'], points['TORSO'])
+def draw_timeseries(point, point_name=''):
+    '''Shows x, y and z timeseries for a given point'''
+    x = point[:,0].T
+    y = point[:,1].T
+    z = point[:,2].T
+    time = list(range(len(z)))
+
+    fig_x = go.Figure(data=go.Scatter(x=time, y=x, mode='markers+lines'), layout=go.Layout(title=f'Point {point_name} X over time', xaxis_title='Frame', yaxis_title='X'))
+    fig_y = go.Figure(data=go.Scatter(x=time, y=y, mode='markers+lines'), layout=go.Layout(title=f'Point {point_name} Y over time', xaxis_title='Frame', yaxis_title='Y'))
+    fig_z = go.Figure(data=go.Scatter(x=time, y=z, mode='markers+lines'), layout=go.Layout(title=f'Point {point_name} Z over time', xaxis_title='Frame', yaxis_title='Z'))
+
+    fig_x.show()
+    fig_y.show()
+    fig_z.show()
+
+
+
+
+
+
+points, COMs, axes = read_Mitchell_data()
+
+draw_timeseries(points['LHM2'], 'LHM2')
+
+
+dfs, labels = filter_points_to_draw(points, COMs)
+invis_dfs = filter_axis_to_draw(axes)
+main_plot = base_plot(dfs, labels, invis_dfs)
+main_plot = draw_line(main_plot, COMs['PELVIS'], COMs['TORSO'])
 main_plot = draw_line(main_plot, points['LHM2'], points['RHM2'])
+#main_plot = draw_anat_ax(main_plot, axes, COMs)
+
 
 main_plot.show()
